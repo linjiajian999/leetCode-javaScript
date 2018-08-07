a6976c5956f0303ed87ad510e474c1e66398b7a1
linjiajian999 <724159997@qq.com>
2018-08-07 14:10:31 +0800

    String: add 709. To Lower Case

---
+++ String/709. To Lower Case.js
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/to-lower-case/description/
+/**
+ * @param {string} str
+ * @return {string}
+ */
+var toLowerCase = function(str) {
+  return str.toLowerCase();
+};

c779a49e597f120931afee597a74d8bb36c24c13
linjiajian999 <724159997@qq.com>
2018-08-08 15:26:44 +0800

    String: update 804

--- String/804. Unique Morse Code Words.js
+++ String/804. Unique Morse Code Words.js
@@ -4,14 +4,28 @@
  * @return {number}
  */
 const m = ['.-','-...','-.-.','-..','.','..-.','--.','....','..','.---','-.-','.-..','--','-.','---','.--.','--.-','.-.','...','-','..-','...-','.--','-..-','-.--','--..'];
+const letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+const lettersMap = new Map();
+letters.forEach((value, index) => { lettersMap.set(value, m[index]) })
 var uniqueMorseRepresentations = function(words) {
-  const map = new Map();
+  const set = new Set();
+  for (let i = 0; i < words.length; i++) {
+    let trans = '';
+    for (let j = 0; j < words[i].length; j++) {
+      trans += lettersMap.get(words[i][j]);
+    }
+    set.add(trans)
+  }
+  return set.size;
+};
+var uniqueMorseRepresentations = function(words) {
+  const set = new Set();
   for (let i = 0; i < words.length; i++) {
     let trans = '';
     for (let j = 0; j < words[i].length; j++) {
       trans += m[words[i][j].charCodeAt() - 97];
     }
-    map.set(trans, true);
+    set.add(trans);
   }
-  return map.size;
+  return set.size;
 };

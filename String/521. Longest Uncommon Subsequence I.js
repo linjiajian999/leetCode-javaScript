dd60747e03b507d1a828e0e141dd85c2b4ac66c2
linjiajian999 <724159997@qq.com>
2018-08-09 01:04:47 +0800

    String: add 521. Longest Uncommon Subsequence I

---
+++ String/521. Longest Uncommon Subsequence I.js
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/longest-uncommon-subsequence-i/description/
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {number}
+ */
+var findLUSlength = function(a, b) {
+  return a === b ? -1 : Math.max(a.length, b.length);
+};

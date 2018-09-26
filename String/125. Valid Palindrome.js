1f81894950a7ea3750564636d9cb43cc806524f4
linjiajian999 <724159997@qq.com>
2018-09-27 01:09:32 +0800

    String: 125. Valid Palindrome change replace case

--- String/125. Valid Palindrome.js
+++ String/125. Valid Palindrome.js
@@ -5,7 +5,7 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-  s = s.toLowerCase().replace(/[^a-z]/g, '');
+  s = s.toLowerCase().replace(/[^a-z0-9]/g, '');
   let left = 0;
   let right = s.length - 1;
   while (left < right) {

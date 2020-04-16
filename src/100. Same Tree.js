7885414f276210b16613f6da773e0a9c633e59e2
linjiajian999 <724159997@qq.com>
2020-04-17 00:56:52 +0800

    100. Same Tree

---
+++ src/100. Same Tree.js
@@ -0,0 +1,23 @@
+/**
+ * 100. Same Tree
+ * https://leetcode.com/problems/same-tree/
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function (p, q) {
+  if (!p && !q) {
+    return true;
+  }
+  if (!p || !q) {
+    return false;
+  }
+  return p.val === q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};

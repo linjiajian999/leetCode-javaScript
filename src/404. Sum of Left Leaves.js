ade452c3bb93b9a31d656e33ad54e12be47db83a
linjiajian999 <724159997@qq.com>
2020-04-18 00:23:15 +0800

    404. Sum of Left Leaves

---
+++ src/404. Sum of Left Leaves.js
@@ -0,0 +1,22 @@
+/**
+ * 404. Sum of Left Leaves
+ * https://leetcode.com/problems/sum-of-left-leaves/
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumOfLeftLeaves = function (root) {
+  if (root === null) {
+    return 0;
+  }
+  if (root.left !== null && root.left.left === null && root.left.right === null) {
+    return root.left.val + sumOfLeftLeaves(root.right)
+  }
+  return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+};
